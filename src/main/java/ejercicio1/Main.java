f4caaba1ba8c18ca1307765f811be31f7a346ab0
obernal-projects <52865391+obernal-projects@users.noreply.github.com>
2020-08-01 17:30:23 +0200

    primer ejercicio Java
    Es la primera subida de la rama.

---
+++ src/main/java/ejercicio1/Main.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercicio1;
+
+/**
+ *
+ * @author Pc
+ */
+public class Main {
+    
+    public static void main(String[]args){
+        
+        // Creamos una instancia (objeto) de la clase vehiculo
+        Vehiculo primerCoche = new Vehiculo();
+        
+        primerCoche.encender();
+        primerCoche.apagar();
+        primerCoche.avanzar();
+        
+        Vehiculo segundoCoche = new Vehiculo();
+        
+    }
+    
+}
---
+++ src/main/java/ejercicio1/Vehiculo.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercicio1;
+
+/**
+ *
+ * @author Pc
+ */
+public class Vehiculo {
+    
+    //Definimos atributos de la clase Vehiculo
+    
+    
+    String marca="Ford";
+    String modelo="Focus";
+    int potencia=111;
+    
+    //Definimos los metodos (acciones que realizamos sobre el objeto
+    
+public void encender(){
+    
+    System.out.println("El coche esta encendido");
+    
+}
+
+public void apagar(){
+    
+    System.out.println("El coche esta apagado");
+    
+}   
+
+public void avanzar(){
+    
+    System.out.println("El coche esta avanzando");
+    
+}
+
+}
